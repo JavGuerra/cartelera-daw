e4b837ae54a214bc5f4d0615d1e4b677da000ca0
JavGuerra <javguerra@badared.com>
2024-06-21 17:26:28 +0200

    Refactoring

--- src/main/java/com/carteleradaw/springboot/web/app/services/IFilmService.java
+++ src/main/java/com/carteleradaw/springboot/web/app/services/IFilmService.java
@@ -53,11 +53,11 @@ public interface IFilmService {
      */
     Page<Film> findAllByCity(String city, Pageable paging);
 
-    /**
-     * Obtiene la lista de películas con el género dado.
-     * @param genre el género.
-     * @return Lista de películas.
-     */
+//    /**
+//     * Obtiene la lista de películas con el género dado.
+//     * @param genre el género.
+//     * @return Lista de películas.
+//     */
 //     List<Film> findAllByGenre(String genre);
 
     /**
