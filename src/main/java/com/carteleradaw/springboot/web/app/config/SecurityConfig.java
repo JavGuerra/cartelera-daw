9905a8c54858b608e0d456c00022af6df12a1097
JavGuerra <javguerra@badared.com>
2024-06-25 19:59:20 +0200

    Actualización de dependencias

--- src/main/java/com/carteleradaw/springboot/web/app/config/SecurityConfig.java
+++ src/main/java/com/carteleradaw/springboot/web/app/config/SecurityConfig.java
@@ -30,33 +30,30 @@ public class  SecurityConfig {
 
         return http
             .authorizeHttpRequests()
-            .requestMatchers("/", "/favicon.ico", "/css/**", "/js/**", "/img/**", "/webjars/**",
-                    "/legal", "/privacy").permitAll()
+            .requestMatchers(HttpMethod.GET,"/", "/favicon.ico", "/legal", "/privacy",
+                    "/css/**", "/js/**", "/img/**", "/webjars/**").permitAll()
 
             .requestMatchers(HttpMethod.GET,"/help").authenticated()
+
             .requestMatchers(HttpMethod.GET,"/cinemas/create").authenticated()
             .requestMatchers(HttpMethod.GET,"/cinemas/{id}/edit").authenticated()
             .requestMatchers(HttpMethod.GET,"/cinemas/{id}/delete").authenticated()
             .requestMatchers(HttpMethod.GET,"/cinemas/**").permitAll()
-            .requestMatchers(HttpMethod.GET,"/cinema/**").permitAll()
 
             .requestMatchers(HttpMethod.GET,"/rooms/create").authenticated()
             .requestMatchers(HttpMethod.GET,"/rooms/{id}/edit").authenticated()
             .requestMatchers(HttpMethod.GET,"/rooms/{id}/delete").authenticated()
             .requestMatchers(HttpMethod.GET,"/rooms/**").permitAll()
-            .requestMatchers(HttpMethod.GET,"/room/**").permitAll()
 
             .requestMatchers(HttpMethod.GET,"/films/create").authenticated()
             .requestMatchers(HttpMethod.GET,"/films/{id}/edit").authenticated()
             .requestMatchers(HttpMethod.GET,"/films/{id}/delete").authenticated()
             .requestMatchers(HttpMethod.GET,"/films/**").permitAll()
-            .requestMatchers(HttpMethod.GET,"/film/**").permitAll()
 
             .requestMatchers(HttpMethod.GET,"/users/create").authenticated()
             .requestMatchers(HttpMethod.GET,"/users/{id}/edit").authenticated()
             .requestMatchers(HttpMethod.GET,"/users/{id}/delete").authenticated()
             .requestMatchers(HttpMethod.GET,"/users/**").authenticated()
-            .requestMatchers(HttpMethod.GET,"/user/**").authenticated()
 
             .anyRequest().permitAll()
 
