a001912fe9da46ce220384ce787528736f0825e6
JavGuerra <javguerra@badared.com>
2024-07-12 18:16:17 +0200

    Refactoring

--- src/main/java/com/carteleradaw/springboot/web/app/controllers/LoginController.java
+++ src/main/java/com/carteleradaw/springboot/web/app/controllers/LoginController.java
@@ -5,16 +5,11 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 
 @Slf4j
